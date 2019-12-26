25304bf53f967cd99255ecfbefdfc2cecf6daac9
biquanyan001 <biquanyan@fareastone.com.tw>
2019-12-26 15:46:02 +0800

    first commit

---
+++ src/main/java/com/fet/xml2json/Xml2jsonApplication.java
@@ -0,0 +1,13 @@
+package com.fet.xml2json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xml2jsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Xml2jsonApplication.class, args);
+	}
+
+}
